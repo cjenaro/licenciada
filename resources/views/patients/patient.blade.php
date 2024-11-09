3f7241790f8c850a83245ff74cc0dd92de95abf4
Jenaro Calviño <jen.calvineo@gmail.com>
2024-11-09 10:27:08 -0300

    patient nav

--- resources/views/patients/patient.blade.php
+++ resources/views/patients/patient.blade.php
@@ -1,8 +1,12 @@
 <x-app-layout>
   <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-      {{ __('layout.patient') }}
-    </h2>
+    <div class="flex items-end justify-between">
+      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        {{ __('layout.patient') }}
+      </h2>
+
+      <a href="{{ route('patients.new' )}}" class="px-4 py-2 bg-foreground/80 text-black rounded-lg hover:bg-foreground/90">&plus;&nbsp;&nbsp;{{ __('patients.add_patient') }}</a>
+    </div>
   </x-slot>
 
   <div class="py-12">
